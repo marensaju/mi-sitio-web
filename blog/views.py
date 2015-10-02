ae2ed1bc62b9ac00f71d73954a33a38f0ecad186
Mario Juarez <marensaju@hotmail.com>
2015-10-02 10:20:31 -0600

    Se creo la primera vista, y se creo la primera plantilla en HTML.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Postear
 
 def listar_publicaciones(request):
-    return render(request, 'blog/listar_publicaciones.html', {})
+    publicaciones = Postear.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
+    return render(request, 'blog/listar_publicaciones.html', {'publicaciones':publicaciones})
