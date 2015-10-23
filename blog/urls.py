ec0d7d1f6d1bab54e7b355a03a26f213e448c3c8
Mario Juarez <marensaju@hotmail.com>
2015-10-23 12:00:57 -0600

    POr fin termine esto jijiji

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
 
      url(r'^$', views.listar_publicaciones),
      url(r'^post/(?P<pk>[0-9]+)/$', views.detalle_publicacion),
+     url(r'^post/new/$', views.nueva_publicacion, name='nueva_publicacion'),
+     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_editar, name='post_editar'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Postear
 from django.shortcuts import get_object_or_404
+from .forms import PostForm
+from django.shortcuts import redirect
 
 
 
@@ -12,3 +14,27 @@ def listar_publicaciones(request):
 def detalle_publicacion(request, pk):
     publicacion = get_object_or_404(Postear, pk=pk)
     return render(request, 'blog/detalle_publicacion.html', {'publicacion': publicacion})
+
+def nueva_publicacion(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.autor = request.user
+            post.save()
+            return redirect('blog.views.detalle_publicacion', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_editar.html', {'form': form})
+def post_editar(request, pk):
+    post = get_object_or_404(Postear, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.autor = request.user
+            post.save()
+            return redirect('blog.views.detalle_publicacion', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_editar.html', {'form': form})
